690a9c0770b4bb94124bda6020af9959dbe41efc
SreeHarsha-07 <iam.harshagowda@gmail.com>
2022-11-29 15:34:58 +0530

    first commit

---
+++ src/app/display/display.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss']
+})
+export class DisplayComponent {
+  @Input() usernamee ="";
+  @Input() emailid ="";
+}
